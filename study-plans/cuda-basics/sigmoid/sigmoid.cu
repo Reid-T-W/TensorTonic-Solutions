d53775cc61727013620b328c12dcf7c7088e4869
Rediet Tadesse Woldeyes <rediettadesse100@gmail.com>
2026-08-02 23:07:03 +0300

    Solve sigmoid

---
+++ study-plans/cuda-basics/sigmoid/sigmoid.cu
@@ -0,0 +1,19 @@
+#include <cuda_runtime.h>
+#include <math.h>
+
+__global__ void sigmoid_kernel(const float* input, float* output, int N) {
+    // Write code here
+    int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (thread_id < N) {
+        output[thread_id] = 1.0 / (1.0 + std::exp(-input[thread_id]));
+    }
+     
+}
+
+extern "C" void solve(const float* input, float* output, int N) {
+    int threads = 256;
+    int blocks = (N + threads - 1) / threads;
+    sigmoid_kernel<<<blocks, threads>>>(input, output, N);
+    cudaDeviceSynchronize();
+}
